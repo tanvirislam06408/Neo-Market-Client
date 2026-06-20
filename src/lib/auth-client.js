631d188ead567b4cc555165c2eca51daab53a3b5
tanvir <mstanvirislam05@gmail.com>
2026-06-20 18:22:53 +0600

    refactor: remove admin client and clean up authentication plugins

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,15 +1,8 @@
 import { jwtClient } from "better-auth/client/plugins"
-import { adminClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: process.env.BETTER_AUTH_URL,
-
-
-     plugins: [
-    jwtClient() ,
-     adminClient() 
-  ]
+    plugins: [jwtClient()],
 })
 
-export const { signIn, signUp, useSession } = createAuthClient()
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import {  admin, jwt } from "better-auth/plugins";
+import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db('monkey');
@@ -26,7 +26,9 @@ export const auth = betterAuth({
   user: {
     additionalFields: {
       role: {
-        defaultValue: 'buyer'
+        type: ["buyer", "seller"],
+        defaultValue: "buyer",
+        input: true,
       },
       status: {
         defaultValue: 'active'
@@ -40,11 +42,7 @@ export const auth = betterAuth({
     }
   },
 
-  plugins: [
-    jwt(),
-    admin()
-  ]
-  ,
+  plugins: [jwt()],
  
   session: {
     cookieCache: {
